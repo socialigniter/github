7dae270c19c4775a037a7c2e6331dbd24a6366a0
Brennan Novak <brennan@reverseproductions.com>
2011-04-18 13:08:37 -0700

    made app install controller method

---
+++ config/install.php
@@ -0,0 +1,19 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Name:			Social Igniter : Github : Install
+* Author: 		Brennan Novak
+* 		  		contact@social-igniter.com
+*         		@brennannovak
+*          
+* Created: 		Brennan Novak
+*
+* Project:		http://social-igniter.com/
+* Source: 		http://github.com/socialigniter/github
+*
+* Description: 	Install values for Github App for Social Igniter 
+
+/* Settings */
+$config['github_settings']['enabled']				= 'TRUE';
+$config['github_settings']['date_style']			= 'SIMPLE';
+$config['github_settings']['comments_allow']		= 'TRUE';
+$config['github_settings']['comments_per_page']	= '5';
